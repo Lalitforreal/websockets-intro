01f5c07fdbe4d2aadf8b091c6e46a237a15eb216
Lalitforreal <s24cseu0093@bennett.edu.in>
2026-01-22 13:09:20 +0530

    did the leave and disconnect logic + cleanup

--- rooms/index.js
+++ rooms/index.js
@@ -18,25 +18,39 @@ app.get("/client2", (req, res) => {
 
 io.on("connection",(socket)=>{
     console.log("User Connected", socket.id);
+
     //send message to that user only 
     socket.emit("welcome","Welcome user ji" + socket.id);
-
+    
     socket.on("join-room",(roomId)=>{
+        // save roomId for later disconnect use
+        socket.data.roomId = roomId; 
+
         socket.join(roomId);
         console.log(`${socket.id} joined room ${roomId}`);
-
-        //for confirmation 
-        //When a user joins, you should emit a confirmation event to the client:
-        socket.emit("joined-room", roomId);
+         // notify others someone joined
+         socket.to(roomId).emit("user-joined", { id: socket.id });
+         
+         //for confirmation 
+         //When a user joins, you should emit a confirmation event to the client:
+         socket.emit("joined-room", roomId);
     });
 
-    socket.on("send-to-room",({roomId, msg})=>{
+    socket.on("send-to-room",({msg})=>{
+        const roomId = socket.data.roomId;
         console.log("room msg", roomId, msg);
+
+
+        if (!roomId) {
+        socket.emit("error", "You are not in any room");
+        return;
+        }
 
         io.to(roomId).emit("room-message: ", {
             msg,
             from: socket.id,
-            roomId
+            roomId,
+            time: Date.now()
         }
          );
           // OR: send to others only (excluding sender)
@@ -44,7 +58,29 @@ io.on("connection",(socket)=>{
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
+        const roomId = socket.data.roomId;
+
+        if (roomId) {
+            io.to(roomId).emit("user-disconnected", {
+            id: socket.id
+            });
+        }
+        });
+
+    socket.on("leave-room", (data) => {
+        console.log("User left the room : ", socket.id);
+        const roomId = socket.data.roomId;
+        socket.leave(roomId)
+        socket.data.roomId = null
+
+        if(roomId){
+            io.to(roomId).emit("user-left",{
+                id : socket.id
+            });
+        }else{
+            return;
+        }
+
     });
 });
 
